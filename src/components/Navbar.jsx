d3275aa47be1e082626c671c88d73890ca142281
Erudite885 <erudite885@gmail.com>
2023-08-07 08:41:05 +0100

    navbar responsive styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,16 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <Link to="/">
-      <div className="bg-black text-white flex items-center px-3 py-8 gap-3">
-        <img src="../cryptocurrency.png" alt="" className="w-10 h-15" />
-        <h2 className="font-bold text-2xl uppercase">Cryptomania</h2>
+      <div className="bg-black text-white flex items-center px-3 py-4 sm:py-8 gap-3">
+        <img
+          src="../cryptocurrency.png"
+          alt=""
+          className="hidden sm:block w-10 h-15"
+        />
+        <div className="wrapper ">
+          <div className="bg ">Cryptomania</div>
+          <div className="fg">Cryptomania</div>
+        </div>
       </div>
     </Link>
   );
