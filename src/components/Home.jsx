2739edfe5835e501177d5c5a96c97d8a093575d8
Erudite885 <erudite885@gmail.com>
2023-08-08 07:41:04 +0100

    home component with antdesign added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Col, Row, Statistic, Typography } from "antd";
+
+import { Cryptocurrencies, News } from "./index";
+
+const { Title } = Typography;
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Title className="heading">Global Crypto Stats</Title>
+      <Row gutter={[32, 32]}>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value={"5"} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value={"5"} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap:" value={"5"} />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Volume" value={"5"} />
+        </Col>
 
-export default Home
+        <Col span={12}>
+          <Statistic title="Total Markets" value={"5"} />
+        </Col>
+      </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 CryptoCurrencies in the World
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest CryptoCurrency News
+        </Title>
+        <Title level={3}>
+          <Link to="/news">Show more</Link>
+        </Title>
+      </div>
+      <News />
+    </>
+  );
+};
+
+export default Home;
