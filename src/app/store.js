6613a77011da222283925b26abee8b855b35dbb1
Erudite885 <erudite885@gmail.com>
2023-08-08 16:07:02 +0100

    crypto api services and middleware added

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { cryptoApi } from "../services/cryptoApi";
 
 export default configureStore({
   reducer: {
-    // [c.reducerPath]: c.reducer,
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
     // [cn.reducerPath]: cn.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(cryptoApi.middleware),
 });
