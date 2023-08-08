dbe24ac8be849f4d7a4d19e15a7faf3a1ca38bea
Erudite885 <erudite885@gmail.com>
2023-08-08 16:02:26 +0100

    exchanges api service created

---
+++ src/services/cryptoExchangesApi.js
@@ -0,0 +1,21 @@
+// const geckoCryptoApiHeaders = {
+//   "X-RapidAPI-Host": "coingecko.p.rapidapi.com",
+//   "X-RapidAPI-Key": "f4e3410a1cmsh7f4625a976971f1p161eacjsncb342f56e0ab",
+// };
+
+
+// const createRequest = (url) => ({ url, headers: geckoCryptoApiHeaders });
+
+// const geckoBaseUrl = "https://coingecko.p.rapidapi.com";
+
+
+
+// export const cryptoApi = createApi({
+//   reducerPath: "cryptoApi",
+//   baseQuery: fetchBaseQuery({ geckoBaseUrl }),
+//   endpoints: (builder) => ({
+//     getCryptoExchange: builder.query({
+//       query: () => createRequest('/exchanges')
+//     }),
+//   }),
+// });
