de5c9701b8c7dbd20f2fa777fcd3839e2a2838bd
Erudite885 <erudite885@gmail.com>
2023-08-08 07:43:48 +0100

    app folder added

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+export default configureStore({
+  reducer: {
+    // [c.reducerPath]: c.reducer,
+    // [cn.reducerPath]: cn.reducer,
+  },
+});
