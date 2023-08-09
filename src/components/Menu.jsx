68ef9cbbdbf3c0c7913d56a1203ac202e047fbad
Erudite885 <erudite885@gmail.com>
2023-08-09 13:01:25 +0100

    menu responsive styling fixed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 import { menu } from "../data";
 const Menu = () => {
+  // console.log(menu.map((item) => item));
   return (
-    <div className="menu h-full bg-black bg-opacity-95 text-white">
-      {menu.map((item) => (
-        <div className="item flex flex-col" key={item.id}>
-          <span className="title hidden md:block text-xs font-extralight uppercase">{item.title}</span>
-          {item.listItems.map((listItem) => (
-            <Link to={listItem.url} className="listItem hover:bg-gradient-to-r hover:from-yellow-600 flex items-center gap-2.5 p-2.5 rounded-md" key={listItem.id}>
-              <img src={listItem.icon} alt="listitem icon" />
-              <span className="hidden md:block listItemTitle">{listItem.title}</span>
+    <div className="mcontainer flex items-center lg:items-start lg:justify-start justify-center text-white relative py-4 lg:py-0 lg:h-full ">
+      <div className="menu lg:py-4 flex bg-black items-center gap-4 overflow-scroll lg:items-start lg:justify-start lg:flex-col lg:h-full ">
+        {menu.map((item) => (
+          <div
+            className="item w-full hover:bg-gradient-to-r hover:from-gray-700 lg:hover:from-yellow-700 "
+            key={item.id}
+          >
+            <Link
+              to={item.url}
+              className="listItem flex gap-3 p-3 rounded-md"
+              key={item.id}
+            >
+              <img src={item.icon} alt={`${item.title} icon`} />
+              <span className="listItemTitle hidden md:block ">
+                {item.title}
+              </span>
             </Link>
-          ))}
-        </div>
-      ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default Menu
+export default Menu;
