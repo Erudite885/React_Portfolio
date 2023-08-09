c5879a7a1593e50e9fe022b91ea637fa2fec9f59
Erudite885 <erudite885@gmail.com>
2023-08-09 12:56:47 +0100

    menu array fixed

--- src/data.js
+++ src/data.js
@@ -1,39 +1,54 @@
 import { ChartIcon, Element, Home, Post } from "./assets/index";
 
 export const menu = [
+  
   {
     id: 1,
-
-    listItems: [
-      {
-        id: 1,
-        title: "Home",
-        url: "/",
-        icon: Home,
-      },
-    ],
+    title: "Home",
+    url: "/",
+    icon: Home,
   },
   {
     id: 2,
-    listItems: [
-      {
-        id: 2,
-        title: "Cryptocurrencies",
-        url: "/cryptocurrencies",
-        icon: ChartIcon,
-      },
-      {
-        id: 3,
-        title: "Exchanges",
-        url: "/exchanges",
-        icon: Element,
-      },
-      {
-        id: 4,
-        title: "News",
-        url: "/news",
-        icon: Post,
-      },
-    ],
+    title: "Cryptocurrencies",
+    url: "/cryptocurrencies",
+    icon: ChartIcon,
   },
+  {
+    id: 3,
+    title: "Exchanges",
+    url: "/exchanges",
+    icon: Element,
+  },
+  {
+    id: 4,
+    title: "News",
+    url: "/news",
+    icon: Post,
+  },
+  //   ],
+  // },
+  // {
+  //   id: 2,
+  //   listItems: [
+  //     {
+  //       id: 2,
+  //       title: "Cryptocurrencies",
+  //       url: "/cryptocurrencies",
+  //       icon: ChartIcon,
+  //     },
+  //     {
+  //       id: 3,
+  //       title: "Exchanges",
+  //       url: "/exchanges",
+  //       icon: Element,
+  //     },
+  //     {
+  //       id: 4,
+  //       title: "News",
+  //       url: "/news",
+  //       icon: Post,
+  //     },
+  //   ],
+  // },
 ];
