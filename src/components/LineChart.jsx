76fbeb9684719e28be76bf1a1836a3fd05aa73d2
Erudite885 <erudite885@gmail.com>
2023-08-09 01:05:38 +0100

    linechart import fixed

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 import { Col, Row, Typography } from "antd";
-import { data } from "autoprefixer";
 
 const { Title } = Typography;
 
