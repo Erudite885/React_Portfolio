7e437cfefd7a95850bfc3ba196b265107cfa7382
Erudite885 <erudite885@gmail.com>
2023-08-09 13:02:23 +0100

    footer image display fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="footer flex flex-col gap-2 sm:gap-0 md:flex-row items-center justify-between bg-black bg-opacity-95 text-white p-8 ">
       <span className="text-sm">Copyright © 2023</span>
       <span className="flex items-center justify-center gap-2">
-        <img src="../cryptocurrency.png" alt="" className="sm:hidden w-7 h-7" />
+        <img src="../cryptocurrency.png" alt="" className="lg:hidden w-7 h-7" />
         <Link className="font-bold text-xl cursor-pointer" to="/">
           Cryptomania Inc.
         </Link>
