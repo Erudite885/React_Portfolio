6e9acfc0b135c6ed2c4801971989a5c2ee0bc0e8
Erudite885 <erudite885@gmail.com>
2023-08-10 23:53:14 +0100

    headers, baseurl, build query added

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,8 +1,12 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
-const cryptoNewsHeaders = {};
+const cryptoNewsHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Key": "f4e3410a1cmsh7f4625a976971f1p161eacjsncb342f56e0ab",
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+};
 
-const baseUrl = "";
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
@@ -11,9 +15,12 @@ export const cryptoNewsApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
-      query: () => createRequest(``),
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
     }),
   }),
 });
 
-export const { useGetCryptoNewsQuery } = cryptoApi;
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
