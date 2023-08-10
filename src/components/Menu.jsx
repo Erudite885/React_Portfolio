c2c374ed74c6e53a077960daa7f49b966e8cefcf
Erudite885 <erudite885@gmail.com>
2023-08-11 00:04:05 +0100

    bg hovering fixed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { menu } from "../data";
 const Menu = () => {
-  // console.log(menu.map((item) => item));
+ 
   return (
     <div className="mcontainer flex items-center lg:items-start lg:justify-start justify-center text-white relative py-4 lg:py-0 lg:h-full ">
       <div className="menu lg:py-4 flex bg-black items-center gap-4 overflow-scroll lg:items-start lg:justify-start lg:flex-col lg:h-full ">
         {menu.map((item) => (
           <div
-            className="item w-full hover:bg-gradient-to-r hover:from-gray-700 lg:hover:from-yellow-700 "
+            className="item w-full hover:bg-gradient-to-t lg:hover:bg-gradient-to-r hover:from-gray-700 lg:hover:from-yellow-700 "
             key={item.id}
           >
             <Link
