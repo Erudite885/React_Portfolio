cf8eb25fcc17a407dde26bc933f3778f0427c611
Erudite885 <erudite885@gmail.com>
2023-08-11 07:49:38 +0100

    cryptodetails loading error fixed

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -14,7 +14,6 @@ import {
   NumberOutlined,
   ThunderboltOutlined,
 } from "@ant-design/icons";
-
 import {
   useGetCryptoDetailsQuery,
   useGetCryptoHistoryQuery,
@@ -25,18 +24,18 @@ const { Title, Text } = Typography;
 const { Option } = Select;
 
 const CryptoDetails = () => {
-  const { coinId } = useParams();
-  const [timePeriod, setTimePeriod] = useState("1d");
-  const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+  const { id } = useParams();
+  const [timePeriod, setTimePeriod] = useState("7d");
+  const { data, isFetching } = useGetCryptoDetailsQuery(id);
   const { data: coinHistory } = useGetCryptoHistoryQuery({
-    coinId,
+    id,
     timePeriod,
   });
   const cryptoDetails = data?.data?.coin;
 
   if (isFetching) return <Loader />;
 
-  const time = ["3h", "24h"];
+  const time = ["3h", "24h", "7d", "30d", "3m", "1y", "3y", "5y"];
 
   const stats = [
     {
@@ -47,7 +46,9 @@ const CryptoDetails = () => {
     { title: "Rank", value: cryptoDetails?.rank, icon: <NumberOutlined /> },
     {
       title: "24h Volume",
-      value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`,
+      value: `$ ${
+        cryptoDetails["24hVolume"] && millify(cryptoDetails["24hVolume"])
+      }`,
       icon: <ThunderboltOutlined />,
     },
     {
@@ -108,16 +109,15 @@ const CryptoDetails = () => {
     <Col className="coin-detail-container">
       <Col className="coin-heading-container">
         <Title level={2} className="coin-name">
-          {data?.data?.coin.name} ({data?.data?.coin.symbol}) Price
+          {cryptoDetails?.name} ({cryptoDetails?.symbol}) Price
         </Title>
         <p>
-          {cryptoDetails.name} live price in USD. View value statistics, market
-          cap and supply.{" "}
+          {cryptoDetails?.name} live price in US Dollars ($). View value
+          statistics, market cap and supply.{" "}
         </p>
       </Col>
-
       <Select
-        defaultValue={"3h"}
+        defaultValue={"7d"}
         className="select-timeperiod"
         placeholder="Select Timeperiod"
         onChange={(value) => setTimePeriod(value)}
@@ -131,25 +131,24 @@ const CryptoDetails = () => {
         currentPrice={millify(cryptoDetails?.price)}
         coinName={cryptoDetails?.name}
       />
-
       <Col className="stats-container">
         <Col className="coin-value-statistics">
           <Col className="coin-value-statistics-heading">
             <Title level={3} className="coin-details-heading">
-              {cryptoDetails.name} Value Statistics
+              {cryptoDetails?.name} Value Statistics
             </Title>
             <p>
-              An overview showing the statistics of {cryptoDetails.name}, such
+              An overview showing the statistics of {cryptoDetails?.name}, such
               as the base and quote currency, the rank, and trading volume.{" "}
             </p>
           </Col>
-          {stats.map(({ icon, title, value }) => (
-            <Col className="coin-stats">
+          {stats?.map(({ icon, title, value }) => (
+            <Col className="coin-stats" key={title}>
               <Col className="coin-stats-name">
                 <Text>{icon}</Text>
                 <Text>{title}</Text>
               </Col>
-              <Text className="stats">{value}</Text>
+              {value && <Text className="stats">{value}</Text>}
             </Col>
           ))}
         </Col>
