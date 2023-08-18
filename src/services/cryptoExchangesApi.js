5295576f23ab36100ebfa665cf434a623b48ad13
Erudite885 <erudite885@gmail.com>
2023-08-18 02:02:57 +0100

    query fixed

--- src/services/cryptoExchangesApi.js
+++ src/services/cryptoExchangesApi.js
@@ -1,39 +1,17 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
-
-// const geckoCryptoApiHeaders = {
-//   "X-RapidAPI-Host": "coingecko.p.rapidapi.com",
-//   "X-RapidAPI-Key": "f4e3410a1cmsh7f4625a976971f1p161eacjsncb342f56e0ab",
-// };
-
-// const createRequest = (url) => ({ url, headers: geckoCryptoApiHeaders });
-
-// const geckoBaseUrl = "https://coingecko.p.rapidapi.com";
-
-// export const cryptoApi = createApi({
-//   reducerPath: "cryptoApi",
-//   baseQuery: fetchBaseQuery({ geckoBaseUrl }),
-//   endpoints: (builder) => ({
-//     getCryptoExchange: builder.query({
-//       query: () => createRequest('/exchanges')
-//     }),
-//   }),
-// });
-
-const cryptoExchangeHeaders = {
-  "X-RapidAPI-Host": "coingecko.p.rapidapi.com",
-  "X-RapidAPI-Key": "f4e3410a1cmsh7f4625a976971f1p161eacjsncb342f56e0ab",
-};
-
-const createRequest = (url) => ({ url, headers: cryptoExchangeHeaders });
-
-const geckoBaseUrl = "https://coingecko.p.rapidapi.com";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
 export const cryptoExchangeApi = createApi({
   reducerPath: "cryptoExchangeApi",
-  baseQuery: fetchBaseQuery({ geckoBaseUrl }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://api.coingecko.com/api/v3" }),
   endpoints: (builder) => ({
     getCryptoExchange: builder.query({
-      query: () => createRequest("/exchanges"),
+      query: () => {
+        return `coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+      },
     }),
   }),
 });
+
+export const { useGetCryptoExchangeQuery } = cryptoExchangeApi;
+
+console.log();
