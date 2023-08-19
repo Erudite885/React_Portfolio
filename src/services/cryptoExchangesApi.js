52cad1cdc9158b091390bfb8aa90fed51fa71fc6
Erudite885 <erudite885@gmail.com>
2023-08-19 05:44:10 +0100

    market query added

--- src/services/cryptoExchangesApi.js
+++ src/services/cryptoExchangesApi.js
@@ -4,6 +4,11 @@ export const cryptoExchangeApi = createApi({
   reducerPath: "cryptoExchangeApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.coingecko.com/api/v3" }),
   endpoints: (builder) => ({
+    getCryptoMarket: builder.query({
+      query: () => {
+        return `https://api.coingecko.com/api/v3/exchanges`;
+      },
+    }),
     getCryptoExchange: builder.query({
       query: () => {
         return `coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
@@ -12,6 +17,6 @@ export const cryptoExchangeApi = createApi({
   }),
 });
 
-export const { useGetCryptoExchangeQuery } = cryptoExchangeApi;
+export const { useGetCryptoExchangeQuery, useGetCryptoMarketQuery } = cryptoExchangeApi;
 
 console.log();
