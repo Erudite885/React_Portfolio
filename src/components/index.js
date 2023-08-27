1a378e6cacdfbb46263f6a905c270a09115c101f
Erudite885 <erudite885@gmail.com>
2023-08-27 19:03:49 +0100

    modified

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,18 @@
-export { default as Home } from "./Home";
+// import {
+//   EarthCanvas,
+//   BallCanvas,
+//   ComputersCanvas,
+//   StarsCanvas,
+// } from "./canvas";
+
+// export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
+
 export { default as Navbar } from "./Navbar";
-export { default as News } from "./News";
-export { default as Cryptocurrencies } from "./Cryptocurrencies";
-export { default as Exchanges } from "./Exchanges";
-export { default as CryptoDetails } from "./CryptoDetails";
-export { default as Footer } from "./Footer";
-export { default as Menu } from "./Menu";
-export { default as Loader } from "./Loader";
-export { default as LineChart } from "./LineChart";
+export { default as About } from "./About";
+export { default as Contact } from "./Contact";
+export { default as Experience } from "./Experience";
+export { default as Feedbacks } from "./Feedbacks";
+export { default as Hero } from "./Hero";
+export { default as CanvasLoader } from "./Loader";
+export { default as Tech } from "./Tech";
+export { default as Works } from "./Works";
