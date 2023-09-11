7f7245d471ac0f15ec88d13ef9cd87f4819f67ac
Erudite885 <erudite885@gmail.com>
2023-09-11 01:57:50 +0100

    contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,15 +32,14 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs
-      .sendForm(
+    emailjs.send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
-          from_name: form.name,
-          to_name: "Bianca Uche",
+          from_name:form.name,
+          to_name: 'Bianca Uche',
           from_email: form.email,
-          to_email: "uchebianca@gmail.com",
+          to_email: 'uchebianca@gmail.com',
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
